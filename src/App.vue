04ce773aac9f0c69ea7dcb0bd258d443c7bd26e4
Ing. Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2018-01-11 12:58:01 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <center>
+          <div class="form-group">
+            <label for="email">Email address:</label>
+            <input type="email" class="form-control" id="email">
+          </div>
+          <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input type="password" class="form-control" id="pwd">
+          </div>
+          <div class="checkbox">
+            <label><input type="checkbox"> Remember me</label>
+          </div>
+          <button type="submit" class="btn btn-default">Submit</button>
+        </center>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {HTTP} from './http-common';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  created(){
+    HTTP.get(`user`)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(e => {
+      console.log(e)
+    })
+  }
+}
+</script>
+
+<style>
+</style>
